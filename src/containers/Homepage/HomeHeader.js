fe6cffca389b9972360a57302cfefd6028f47b01
Minh <tranminhbvhp@gmail.com>
2024-03-22 00:23:01 +0700

    part 47

--- src/containers/Homepage/HomeHeader.js
+++ src/containers/Homepage/HomeHeader.js
@@ -3,11 +3,17 @@ import { connect } from 'react-redux';
 import './HomeHeader.scss'
 import logo from '../../assets/logo.svg';
 import { FormattedMessage } from 'react-intl';
+import { LANGUAGES } from '../../utils/constant';
+import { changeLanguageApp } from '../../store/actions';
 
 class HomeHeader extends Component {
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language);
+    }
 
     render() {
-        console.log('check props: ', this.props);
+        let language = this.props.language;
+
         return (
             <Fragment>
                 <div className='home-header-container'>
@@ -46,8 +52,8 @@ class HomeHeader extends Component {
                                 <i className="fas fa-question-circle"></i>
                                 <FormattedMessage id="home-header.support" />
                             </div>
-                            <div className='language-vi'>VN</div>
-                            <div className='language-en'>EN</div>
+                            <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}><span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
+                            <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
                         </div>
 
                     </div>
@@ -99,7 +105,7 @@ class HomeHeader extends Component {
                         </div>
                     </div>
                 </div>
-            </Fragment>
+            </Fragment >
 
         );
     }
@@ -115,6 +121,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
     };
 };
 
